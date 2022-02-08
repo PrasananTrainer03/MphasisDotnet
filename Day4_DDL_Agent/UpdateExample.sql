4b9936884994d44788c3cd8b089f738192efd2ca
Prasanna <prassup.dotnet@gmail.com>
2022-02-08 12:34:10 +0530

    code

---
+++ Day4_DDL_Agent/UpdateExample.sql
@@ -0,0 +1,11 @@
+/* Update Command */
+
+Used to update the table records, 
+
+Update table_name set field1=value, field2=value {where condition} 
+
+select * from Agent;
+
+Update Agent set FullName = FirstName + ' ' + MI + ' ' + LastName
+GO
+
